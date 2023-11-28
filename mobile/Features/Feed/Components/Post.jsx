309a2b37bf5fdcbc520742f0fbe0ff3815121495
Antonio Cosio <coshio@g.ucla.edu>
2023-11-27 22:17:36 -0800

    first milestone of returner onboarding

---
+++ mobile/Features/Feed/Components/Post.jsx
@@ -0,0 +1,24 @@
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Post({ username, body }) {
+  return (
+    <>
+      <Text>
+        Author:
+        {' '}
+        {username}
+      </Text>
+      <Text>
+        Body:
+        {' '}
+        {body}
+      </Text>
+    </>
+  );
+}
+
+Post.propTypes = {
+  username: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
